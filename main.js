65d6ff3e1917f615f89ce620fd95acc27fda7e86
VOLKOVskiyy <64324232+VOLKOVskiyy@users.noreply.github.com>
2021-05-31 17:31:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function tem() {
+		document.getElementById('shap1').style.backgroundColor = document.getElementById('shap1').style.backgroundColor == 'var(--red)' ? '#0066ff' : 'var(--red)'
+		document.getElementById('shap2').style.backgroundColor = document.getElementById('shap2').style.backgroundColor == 'var(--red)' ? '#0066ff' : 'var(--red)'
+		document.getElementById('bodyg').style.backgroundImage = document.getElementById('bodyg').style.backgroundImage == 'url(img/bg2red.png)' ? 'url(img/bg2.png)' : 'url(img/bg2red.png)'
+	}
+	function uvedom(id) {
+		document.getElementById(id).style.display = 'inline-block'
+		setTimeout(function() {
+			document.getElementById(id).style.opacity = '1'
+			document.getElementById(id).style.transform = 'translate(-50%) translateY(0%)'
+		}, 10);
+		setTimeout(function() {
+			document.getElementById(id).style.opacity = '0'
+			document.getElementById(id).style.transform = 'translate(-50%) translateY(100%)'
+		}, 1500);
+		setTimeout(function() {
+			document.getElementById(id).style.display = 'none'
+		}, 1750);
+	}
+	function mainP(locationHref) {
+		document.getElementById('shap2').style.height = '100%'
+		setTimeout(function() {
+			location.href = locationHref
+		}, 1000);
+	}
+	function strelka() {
+		document.getElementById('strelka').style.left = '-50%'
+		setTimeout(function() {
+			location.href = 'index.html'
+		}, 1500);
+	}
+
+	function openM(id) {
+		document.getElementById(id).style.display = 'block'
+		document.getElementById('fon').style.display = 'block'
+		setTimeout(function() {
+			document.getElementById(id).style.transform = 'translate(-50%) translateY(0%)'
+			document.getElementById('fon').style.opacity = '.5'
+		}, 10);
+	}
+	function closedM(id) {
+		document.getElementById(id).style.transform = 'translate(-50%) translateY(100%)'
+		document.getElementById('fon').style.opacity = '0'
+		setTimeout(function() {
+			document.getElementById(id).style.display = 'none'
+		document.getElementById('fon').style.display = 'none'
+		}, 300);
+	}
